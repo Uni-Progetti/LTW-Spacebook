6088da20a7686e1645e6b48df5b29c3dd267263d
Matteo-Tedesco <tedesco.1635912@studenti.uniroma1.it>
2023-05-27 11:36:38 +0200

    Aggiunto controllo autorizzazioni e sanificazione parametri devise ad application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller? # Aggiunge parametri al controllo di sicurezza di devise
     helper_method :require_department
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+          user_params.permit( :email, :password, :password_confirmation, :role, :requested_manager)  # Parametri permessi DEVISE (la struttura passata al metodo permit è un array)
+        end
+    end
+
+    rescue_from CanCan::AccessDenied do |exception|                                                  # Reindirizza alla pagina corrente in caso di accesso negato da parte di Canard
+        redirect_back(fallback_location: root_path)                                                  # in caso di errore di path reindirizza alla home 
+        flash[:alert] = exception.message                                                            # Mostra messagio di errore
+    end
 
     # rescue_from SQLite3::ConstraintException do |exception|                                          # Reindirizza alla pagina corrente se i dati inseriti non rispettano i vincoli sul database
     #     redirect_back(fallback_location: root_path)                                                  # in caso di errore di path reindirizza alla home
